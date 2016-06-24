f45e19bbbf761d38c6b4d8445fffa53bf35e6a2c
Ramon Fuentes Franco <rfuentes@ictp.it>
2016-06-24 19:00:45 +0200

    Removed basemap from setup.py requirements.
    Basemap can at the moment apparently not be installed easily via pip.
    (see https://github.com/matplotlib/basemap/issues/251 and https://github.com/matplotlib/basemap/issues/198)
    
    Signed-off-by: Ramon Fuentes Franco <rfuentes@ictp.it>

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['basemap',
+    install_requires=[# 'basemap',
                       'matplotlib',
                       'netCDF4',
                       'numpy',
